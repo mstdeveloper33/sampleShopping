f0baa6ca327ef61720b7a7628ac86e8916b6260f
mstdeveloper33 <mst.ttr02@gmail.com>
2024-07-10 15:39:32 +0300

    firebase bildirim calismasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sampleshopping/home_page.dart';
+import 'package:sampleshopping/firebase_options.dart';
+
 import 'package:sampleshopping/intro_page.dart';
 import 'package:sampleshopping/model/cart_model.dart';
+import 'package:sampleshopping/service/firebase_api.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseApi().initNotifications();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
